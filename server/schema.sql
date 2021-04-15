7db0e02bc27b13bd69838035503f0aee0850039a
Jesse Parker <senorjp@gmail.com>
2021-04-15 20:47:52 +0000

    nodejs/mysql server beginnings

---
+++ server/schema.sql
@@ -0,0 +1,23 @@
+drop table if exists things;
+create table things (
+	id int(11) not null auto_increment primary key,
+	thing_id int(11) not null,
+	user_id int(11) not null,
+	name varchar(100) null default '',
+	imageFile varchar(100) null default '',
+	parentId int(11) null default null,
+	print tinyint(1) null default 0,
+	`type` varchar(20) null default '',
+	sku_min_qty int(11) null default null,
+	sku_qty int(11) null default null,
+	qty int(11) null default null,
+	type_data varchar(200) null default '',
+	rack_rows int(11) null default null,
+	rack_cols int(11) null default null,
+	rack_position varchar(4) null default '',
+	imageData text null default null,
+	time_created int(11) null default null,
+	time_modified int(11) null default null,
+	time_scanned int(11) null default null
+);
+
---
+++ server/server.js
@@ -0,0 +1,76 @@
+//Parse data from JSON POST and insert into MYSQL
+
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+var mysql = require('mysql');
+
+// Configure MySQL connection
+var connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'thing',
+	password: 'porcupine',
+	database: 'thing'
+  })
+
+//Establish MySQL connection
+connection.connect(function(err) {
+   if (err) 
+      throw err
+   else {
+       console.log('Connected to MySQL');
+       // Start the app when connection is ready
+       app.listen(3000);
+       console.log('Server listening on port 3000');
+ }
+});
+
+app.use(bodyParser.json())
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname+ '/myfile.html'));
+});
+
+app.post('/', function(req, res) {
+
+var jsondata = req.body;
+var values = [];
+
+for(var i=0; i< jsondata.length; i++)
+  values.push([jsondata[i].id, jsondata[i].user_id, jsondata[i].name, jsondata[i].parentId, jsondata[i].type, jsondata[i].sku_min_qty, jsondata[i].sku_qty, jsondata[i].qty, jsondata[i].type_data, jsondata[i].rack_rows, jsondata[i].rack_cols, jsondata[i].rack_position, jsondata[i].imageData]);
+
+
+//Bulk insert using nested array [ [a,b],[c,d] ] will be flattened to (a,b),(c,d)
+connection.query('INSERT INTO things (thing_id, user_id, name, parentId, type, sku_min_qty, sku_qty, qty, type_data, rack_rows, rack_cols, rack_position, imageData) VALUES ?', [values], function(err,result) {
+  if(err) {
+     res.send('Error');
+	  console.log(err);
+  }
+ else {
+     res.send('Success');
+  }
+});
+});
+
+app.post('/updateThing', function(req, res) {
+
+var jsondata = req.body;
+var values = [];
+
+for(var i=0; i< jsondata.length; i++)
+  values = [jsondata[i].name, jsondata[i].parentId, jsondata[i].type, jsondata[i].sku_min_qty, jsondata[i].sku_qty, jsondata[i].qty, jsondata[i].type_data, jsondata[i].rack_rows, jsondata[i].rack_cols, jsondata[i].rack_position, jsondata[i].imageData, jsondata[i].id, jsondata[i].user_id];
+
+
+//Bulk insert using nested array [ [a,b],[c,d] ] will be flattened to (a,b),(c,d)
+connection.query('update things set name = ?, parentId = ?, type = ?, sku_min_qty = ?, sku_qty = ?, qty = ?, type_data = ?, rack_rows = ?, rack_cols = ?, rack_position = ?, imageData = ? where thing_id = ? and user_id = ?', values, function(err,result) {
+  if(err) {
+     res.send('Error');
+          console.log(err);
+  }
+ else {
+     res.send('Success');
+  }
+});
+});
+
